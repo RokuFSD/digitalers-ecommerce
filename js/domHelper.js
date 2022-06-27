0bc67ffe47452dce6ad4ed0d2af1d6aa3ee3d739
RokuFSD <emilianordx@gmail.com>
2022-06-27 16:52:43 -0300

    prueba jess

--- js/domHelper.js
+++ js/domHelper.js
@@ -1,4 +1,5 @@
 /*Funcion que me permite rellenar los campos de un formulario con datos */
+/* */
 export function fillForm(form, data) {
     const entries = Object.entries(data);
     for(const [key, val] of entries){
